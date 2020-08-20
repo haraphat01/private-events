9295d5dec1cbf2e8fad597ac8c9dc4948df3d2df
haraphat01 <haraphat@gmail.com>
2020-08-20 11:01:48 +0100

    Created the controller for the app

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'events/index'
+  get 'events/new'
+  get 'events/show'
+  get 'users/new'
+  get 'users/create'
+  get 'users/show'
    devise_for :users
    root to: "home#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
