97c0f9f4300531581f23b9d0c55ff6999df8900f
haraphat01 <haraphat@gmail.com>
2020-08-22 08:10:08 +0100

    Fixed the failed test

--- spec/features/authentication_spec.rb
+++ spec/features/authentication_spec.rb
@@ -11,13 +11,13 @@ feature 'sign up process' do
       visit new_user_registration_path
       fill_in 'Username', with: 'testing_username'
       fill_in 'Email', with: 'test@example.com'
-      fill_in 'Password', with: 'cake,biscuitsandtea'
-      fill_in 'Password confirmation', with: 'cake,biscuitsandtea'
+      fill_in 'Password', with: 'akataarafat'
+      fill_in 'Password confirmation', with: 'akataarafat'
       click_on 'Sign up'
     end
 
     scenario 'redirects to root path after signup' do
-      expect(page).to have_content 'Logout'
+      expect(page).to have_content 'Log out'
     end
   end
 end
